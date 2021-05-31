11c3aff49ff0001670cc3decfc2100c2150d2d47
vvkt453 <vivek@trendlyne.com>
2021-05-31 10:36:13 +0530

    button and input focus no css, some comments

--- src/App.js
+++ src/App.js
@@ -4,23 +4,27 @@ import './App.css';
 import Button from './components/Button';
 
 const App = () => {
+    // default state of count is set to 1.
     const [count, setCount] = useState(1);
     const maxVal = 1000;
 
+    // Function handles counter increment
     const incrementCountHandler = () => {
       if (count < maxVal) {
         setCount(count + 1);
       }
     }
     
+    // Function handles counter decrement
     const decrementCountHandler = () => {
       setCount(count - 1);
-      console.log('count-', count);
+      // console.log('count-', count);
     }
 
+    // Function handles input change event
     const onInputchange = (event) => {
       setCount(event.target.value);
-      console.log('countc', count);
+      // console.log('countc', count);
     }
 
     return (
